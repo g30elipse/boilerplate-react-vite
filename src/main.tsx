3684e053c827aba3b452cb7f22a9209546884e56
Mukut Brahma <mukutb.mac@gmail.com>
2022-07-04 13:44:09 +0530

    removed header

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
 import './index.scss'
+import Loader from './core/components/Loader';
+import Toast from './core/components/Toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <Loader />
+    <Toast />
     <App />
   </BrowserRouter>
 )
